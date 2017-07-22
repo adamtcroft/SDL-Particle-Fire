3881891c5d459f1473c13501680191747e76f64d
Adam T. Croft <sendtoatc@gmail.com>
2017-07-21 18:14:29 -0700

    Adding all project files (xcopy)

---
+++ COP_FirstProject/main.cpp
@@ -0,0 +1,53 @@
+// First SDL test project from caveofprogramming.com
+
+#include <iostream>
+#include <SDL.h>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "SDL init failed!" << endl;
+		return 1;
+	}
+
+	SDL_Window *window = SDL_CreateWindow(
+		"Particle Fire Explosion",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH,
+		SCREEN_HEIGHT,
+		SDL_WINDOW_SHOWN);
+
+	if (window == NULL)
+	{
+		SDL_Quit();
+		return 2;
+	}
+
+	bool quit = false;
+	SDL_Event event;
+
+	while (!quit)
+	{
+		// Update Particles
+		// Draw Particles
+		// Check for messages/events
+
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+}
